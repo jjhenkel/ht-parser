f024a1c9c7542a3bd9dea1bb7adcbd07d8b93ccb
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2018-08-28 12:06:51 -0500

    Updates to use v2 UASTs and Go client.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,33 @@
 #!/bin/sh
 
-# Make log directory
-mkdir -p ./logs
+# set -ex
+
+# export GRPC_GO_LOG_SEVERITY_LEVEL=info
+# export GRPC_GO_LOG_VERBOSITY_LEVEL=2
 
 # Start drivers
 echo "[ht-parser][${1}] Starting drivers as background processes..."
-/opt/drivers/bash/bin/driver --address /tmp/bash --network unix &> ./logs/bash-driver.txt  & 
+/opt/drivers/bash/bin/driver --address localhost:9432 --log-level debug & 
 echo "[ht-parser][${1}][1/8] Bash driver listening on unix:///tmp/bash"
-/opt/drivers/go/bin/driver --address /tmp/go --network unix &> ./logs/go-driver.txt & 
+/opt/drivers/go/bin/driver --address localhost:9433 --log-level debug & 
 echo "[ht-parser][${1}][2/8] Go driver listening on unix:///tmp/go"
-/opt/drivers/java/bin/driver --address /tmp/java --network unix &> ./logs/java-driver.txt &
+/opt/drivers/java/bin/driver --address localhost:9434 --log-level debug & 
 echo "[ht-parser][${1}][3/8] Java driver listening on unix:///tmp/java"
-/opt/drivers/javascript/bin/driver --address /tmp/javascript --network unix &> ./logs/javascript-driver.txt &
+/opt/drivers/javascript/bin/driver --address localhost:9435 --log-level debug & 
 echo "[ht-parser][${1}][4/8] Javascript driver listening on unix:///tmp/javascript"
-/opt/drivers/php/bin/driver --address /tmp/php --network unix &> ./logs/php-driver.txt &
+/opt/drivers/php/bin/driver --address localhost:9436 --log-level debug & 
 echo "[ht-parser][${1}][5/8] Php driver listening on unix:///tmp/php"
-/opt/drivers/python/bin/driver --address /tmp/python --network unix &> ./logs/python-driver.txt &
+/opt/drivers/python/bin/driver --address localhost:9437 --log-level debug & 
 echo "[ht-parser][${1}][6/8] Python driver listening on unix:///tmp/python"
-/opt/drivers/ruby/bin/driver --address /tmp/ruby --network unix &> ./logs/ruby-driver.txt & 
+/opt/drivers/ruby/bin/driver --address localhost:9438 --log-level debug & 
 echo "[ht-parser][${1}][7/8] Ruby driver listening on unix:///tmp/ruby"
-/opt/drivers/typescript/bin/driver --address /tmp/typescript --network unix &> ./logs/typescript-driver.txt & 
+/opt/drivers/typescript/bin/driver --address localhost:9439 --log-level debug & 
 echo "[ht-parser][${1}][8/8] Typescript driver listening on unix:///tmp/typescript"
 echo "[ht-parser][${1}] Drivers readied."
 
 # Shallow clone
 echo "[ht-parser][${1}] Cloning into /target (shallow clone)..."
-git clone --depth 1 $1 ./target &> ./logs/shallow-clone.txt
+git clone --depth 1 $1 ./target
 echo "[ht-parser][${1}] Shallow clone complete"
 
 # Get into target dir and grab some info
@@ -40,20 +42,17 @@ COMMIT="$(git rev-parse HEAD)"
 echo "[ht-parser][${1}][2/2] CURRENT_COMMIT='${COMMIT}'"
 echo "[ht-parser][${1}] Repo info collected."
 
-# Make output dir for UASTs
-mkdir -p ./repository-uasts
-
 # Run enry and pipe to python app
 echo "[ht-parser][${1}] Running enry + parser..."
-enry -json | python3 /app/glue.py $REPO $BRANCH $COMMIT
+enry -json | python3 /app/glue.py $REPO $BRANCH $COMMIT \
+  >> "../$(basename ${1} .git)-$BRANCH-$COMMIT.tsv"
 echo "[ht-parser][${1}] Enry + parser completed."
 
 # Go up a directory and compress
 cd ../
-tar -zcf "$(basename ${1} .git)-$BRANCH-$COMMIT.tar.gz" ./target/
-tar -zcf "$(basename ${1} .git)-$BRANCH-$COMMIT-logs.tar.gz" ./logs/
+# tar -zcf "$(basename ${1} .git)-$BRANCH-$COMMIT-logs.tar.gz" ./logs/
 
 # Log last bits of info
-echo "[ht-parser][${1}] Produced: $(pwd)$(basename ${1} .git)-$BRANCH-$COMMIT.tar.gz"
-echo "[ht-parser][${1}] Produced: $(pwd)$(basename ${1} .git)-$BRANCH-$COMMIT-logs.tar.gz"
+echo "[ht-parser][${1}] Produced: $(pwd)$(basename ${1} .git)-$BRANCH-$COMMIT.tsv"
+# echo "[ht-parser][${1}] Produced: $(pwd)$(basename ${1} .git)-$BRANCH-$COMMIT-logs.tar.gz"
 echo "[ht-parser][${1}] Finished."
